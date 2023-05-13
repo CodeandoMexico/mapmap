a1a14ab57834c6b99ec760f8bb87a555b9382817
Óscar <2067516+oxcar@users.noreply.github.com>
2023-05-12 23:19:29 -0600

    Scripts de BBDD en una carpeta independiente

--- mapmap-server/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into agency (id, color, gtfsagencyid, lang, name, phone, systemmap, timezone, url)
-values (1, '#222222', 'CMX', 'es', 'Codeando Mexico', '', true, 'GMT-6', 'https://codeandomexico.org');
