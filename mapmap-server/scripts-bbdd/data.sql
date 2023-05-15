65ece95ec34cdad8713182bd54714415b980cd20
Abraham Toriz <categulario@mail.mayfirst.org>
2023-05-15 13:05:49 -0600

    remove unnecessary or hardcoded directives from sql scripts

--- mapmap-server/scripts-bbdd/data.sql
+++ mapmap-server/scripts-bbdd/data.sql
@@ -1,3 +1,2 @@
-\c mapmap
 insert into agency (id, color, gtfsagencyid, lang, name, phone, systemmap, timezone, url)
 values (1, '#000000', 'CMX', 'es', 'Codeando Mexico', '', true, 'GMT-6', 'https://codeandomexico.org');
